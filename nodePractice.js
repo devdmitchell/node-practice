7d20eb720a60b01818df475477c9f5651cbc2ffd
devdmitchell <dmitchell.prsvr@gmail.com>
2024-10-07 11:23:41 -0500

    first draft

--- nodePractice.js
+++ nodePractice.js
@@ -1,2 +1,21 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
+
+
+const firstName = getInput(1);
+const lastName = getInput(2);
+
+
+console.log(`Hello ${firstName} ${lastName}`);
+
+
+const fullNameCapitalized = `${firstName.toUpperCase()} ${lastName.toUpperCase()}`;
+console.log(`${fullNameCapitalized} is your name capitalized.`);
+
+
+const initials = `${firstName.charAt(0)}. ${lastName.charAt(0)}.`;
+console.log(initials);
+
+
+const emailAddress = `${firstName.charAt(0).toLowerCase()}${lastName.toLowerCase()}.prsvr@gmail.com`;
+console.log(emailAddress);

1fa721243389f0e3b197731b73f25f710aed3f03
devdmitchell <dmitchell.prsvr@gmail.com>
2024-10-08 08:24:11 -0500

    submission

--- nodePractice.js
+++ nodePractice.js
@@ -1,21 +1,14 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
 
+const firstName = getInput(1)
+const lastName = getInput(2)
 
-const firstName = getInput(1);
-const lastName = getInput(2);
-
-
-console.log(`Hello ${firstName} ${lastName}`);
-
-
-const fullNameCapitalized = `${firstName.toUpperCase()} ${lastName.toUpperCase()}`;
-console.log(`${fullNameCapitalized} is your name capitalized.`);
-
-
-const initials = `${firstName.charAt(0)}. ${lastName.charAt(0)}.`;
-console.log(initials);
-
-
-const emailAddress = `${firstName.charAt(0).toLowerCase()}${lastName.toLowerCase()}.prsvr@gmail.com`;
-console.log(emailAddress);
+const fullName = (firstName + " " + lastName)
+console.log("Hello " + fullName)
+let capitalName = fullName.toUpperCase()
+console.log(capitalName)
+const initials = firstName [0] + ". " + lastName[0] + ". "
+console.log(initials)
+const email = firstName [0] + lastName + ".prsvr@gmail.com"
+console.log(email)
